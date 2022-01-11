d798a0fda8888c56149ec89bd3b3eda690514764
Nice1Rach <68408670+Nice1Rach@users.noreply.github.com>
2022-01-11 19:50:17 +1300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let loadDone = false;
 
 // Unsplash API
 const apiKey = 'OZW-0mtx97tbtHI6GQAa6e9iSJNfq_yhrfDMCR47A5A';
-const count = 0;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&YOUR_ACCESS_KEY&count=${count}`;
+const count = 30;
 
 // Check if all images were loaded
 function hasImageLoaded() {
